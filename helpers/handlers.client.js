a58d0597a0e0d49aef05c26a3f5d24852c98d152
javimosch <javiermosch@gmail.com>
2016-02-11 16:10:23 +0100

    client handler added

--- helpers/handlers.client.js
+++ helpers/handlers.client.js
@@ -1,42 +1,53 @@
-var mongoose= require('./db').mongoose;
+var mongoose = require('./db').mongoose;
 var generatePassword = require("password-maker");
-var Client  = mongoose.model('Client'); 
+var Client = mongoose.model('Client');
 var promise = require('./utils').promise;
 var validate = require('./validator').validate;
 var handleMissingKeys = require('./validator').handleMissingKeys;
 
-function handleError(res,err){
-    res.json({message:"Server error",detail:err});
+function handleError(err) {
+    console.log('CLIENT:HANDLING-ERROR');
+    return {
+        message: "Server error",
+        detail: err
+    };
 }
 
-function exists(_id){
-    return promise(function(resolve,error){
-        if(_id){
-            Client.find({_id:{$eq:data._id}},function(err,r){
-                if(r && r.length>=1){
+function exists(_id) {
+    return promise(function(resolve, error) {
+        if (_id) {
+            Client.find({
+                _id: {
+                    $eq: _id
+                }
+            }, function(err, r) {
+                if (r && r.length >= 1) {
                     resolve(r[0]);
-                }else{
+                } else {
                     error();
-                } 
+                }
             });
-        }else{
+        } else {
             error();
         }
     });
 }
-function save(data,callback){
+
+function save(data, callback) {
     console.log('CLIENT:SAVE:VALIDATING');
-    validate(data,['email']).error(function(keys){
+    validate(data, ['email']).error(function(keys) {
         console.log('CLIENT:SAVE:VALIDATING:FAIL');
-        return handleMissingKeys(keys,callback);
+        return handleMissingKeys(keys, callback);
     }).then(_check);
-    function _check(){
+
+    function _check() {
         console.log('CLIENT:SAVE:CHECK');
-         exists(data._id).then(_save).error(function(){
+        exists(data._id).then(_save).error(function() {
             _save(undefined);
-        });  
-     }  
-    function _save(instance){
+        });
+    }
+
+    function _save(instance) {
         instance = instance || new Client(data);
         instance.type = data.type || instance.type;
         instance.address = data.address || instance.address;
@@ -46,55 +57,141 @@ function save(data,callback){
         instance.email = data.email || null;
         instance.password = data.password || generatePassword(8);
         console.log('CLIENT:SAVE:SAVING');
-        instance.save(function(err,instance){
-            if(err) return handleError(err);
+        instance.save(function(err, instance) {
+            if (err) return callback(handleError(err));
             console.log('CLIENT:SAVE:SAVING:SUCCESS');
-            callback({message:'Save success',result:instance});
+            callback({
+                message: 'Save success',
+                result: instance
+            });
         });
     }
 }
-function getAll(callback){
-     Client.find(function(err,r){
-         if(err) handleError(err);
-        if(r && r.length>=1){
-            callback({message:'Retrieved success',result:r});
-        }else{
-            callback({message:'Retrieved failed. Item not found.',result:[]});
-        } 
+
+function getAll(callback) {
+    Client.find(function(err, r) {
+        if (err) return callback(handleError(err));
+        if (r && r.length >= 1) {
+            callback({
+                message: 'Retrieved success',
+                result: r
+            });
+        } else {
+            callback({
+                message: 'Retrieved failed. Item not found.',
+                result: []
+            });
+        }
     });
 }
 
 
+function remove(data, callback) {
+    validate(data, ['_id']).error(function(keys) {
+        console.log('CLIENT:REMOVE:VALIDATING:FAIL');
+        return handleMissingKeys(keys, callback);
+    }).then(_remove);
+
+    function _remove() {
+        console.log('CLIENT:REMOVE:BEGIN');
+        Client.remove({
+            _id: {
+                $eq: data._id
+            }
+        }, function(err) {
+            if (err) return callback(handleError(err));
+            console.log('CLIENT:REMOVE:SUCCESS');
+            callback({ok:true,message:'Client deleted',result:null});
+        });
+    }
+}
+
+function get(data, callback) {
+    validate(data, ['_id']).error(function(keys) {
+        console.log('CLIENT:GET:VALIDATING:FAIL');
+        return handleMissingKeys(keys, callback);
+    }).then(_get);
+
+    function _get() {
+        Client.find({
+            _id: {
+                $eq: data._id
+            }
+        }, function(err, r) {
+            if (err) return handleError(err);
+            if (r && r.length >= 1) {
+                callback({
+                    ok:true,
+                    message: 'Retrieved success',
+                    result: r[0]
+                });
+            } else {
+                callback({
+                    ok:false,
+                    message: 'Retrieved failed. Item not found.',
+                    result: null
+                });
+            }
+
+        });
+    }
+}
+
+
+function removeAll(data, callback) {
+    validate(data, ['ids']).error(function(keys) {
+        console.log('CLIENT:REMOVE-ALL:VALIDATING:FAIL');
+        return handleMissingKeys(keys, callback);
+    }).then(_removeAll);
+
+    function _removeAll() {
+        console.log('CLIENT:REMOVE-ALL:BEGIN');
+        Client.remove({
+            _id: {
+                $all: data.ids
+            }
+        }, function(err) {
+            if (err) return callback(handleError(err));
+            console.log('CLIENT:REMOVE-ALL:SUCCESS');
+            callback({ok:true,message:'Clients deleted',result:null});
+        });
+    }
+}
+
 exports.actions = {
-    getAll:getAll,
-    save:save
+    getAll: getAll,
+    save: save,
+    remove: remove,
+    removeAll:removeAll
 };
 
 
-exports.get = function (req,res){
+exports.get = function(req, res) {
     var data = req.body;
-     Diagnostiqueur.find({_id:{$eq:data._id}},function(err,r){
-         if(err) handleError(err);
-        if(r && r.length>=1){
-            res.json({message:'Retrieved success',item:r[0]});
-        }else{
-            res.json({message:'Retrieved failed. Item not found.',item:null});
-        } 
+    get(data, function(rta) {
+        res.json(rta);
     });
 };
 
-exports.getAll = function (req,res){
-    getAll(function(rta){
-       res.json(rta) ;
+exports.getAll = function(req, res) {
+    getAll(function(rta) {
+        res.json(rta);
     });
 };
 
-exports.save = function (req, res) {
+exports.save = function(req, res) {
     var data = req.body;
-    save(data,function(result){
+    save(data, function(result) {
         res.json(result);
     });
 };
-
-
-
+exports.remove = function(req, res) {
+    remove(req.body, function(result) {
+        res.json(result);
+    });
+};
+exports.removeAll = function(req, res) {
+    removeAll(req.body, function(result) {
+        res.json(result);
+    });
+};
--- helpers/routes.js
+++ helpers/routes.js
@@ -2,6 +2,7 @@ var user = require('./handlers.user');
 var test = require('./handlers.testing');
 var inspector = require('./handlers.inspector');
 var order = require('./handlers.order');
+var client = require('./handlers.client');
 
 exports.configure = function(app){
   
@@ -10,12 +11,18 @@ exports.configure = function(app){
     app.post('/login', user.login);
     app.post('/newcat', test.newCat);
     
-    app.post('/save/inspector', inspector.save);
-    app.post('/get/inspector', inspector.get);
-    app.get('/getAll/inspector', inspector.getAll);
-    
+    app.post('/inspector/save', inspector.save);
+    app.post('/inspector/get', inspector.get);
+    app.get('/inspector/getAll', inspector.getAll);
+
+    app.post('/client/save', client.save);
+    app.post('/client/get', client.get);
+    app.post('/client/getAll', client.getAll);
+    app.post('/client/remove', client.remove);
+    app.post('/client/removeAll', client.removeAll);
+
+
     app.post('/save/order', order.save);
-  
-  
+
     console.log('Routes loaded.');
 };

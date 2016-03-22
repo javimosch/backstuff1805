bf02616cedfba70d1a0ec02a78741d833c50bd8f
Javi <arancibiajav@gmail.com>
2016-03-22 11:26:33 +0000

    User#save dummy test added

--- test/test.js
+++ test/test.js
@@ -4,3 +4,11 @@ describe('String#split', function(){
     assert(Array.isArray('a,b,c'.split(',')));
   });
 })
+
+
+var assert = require('assert');
+describe('User#save', function(){
+  it('should save success', function(){
+    assert(Array.isArray('Not an array!'));
+  });
+})

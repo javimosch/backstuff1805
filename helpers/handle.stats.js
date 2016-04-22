1097063a05740d57a0bd6006efe6c1256befd13e
javoche <arancibiajav@gmail.com>
2016-04-22 08:28:19 +0000

    master#balance-updated

--- helpers/handle.stats.js
+++ helpers/handle.stats.js
@@ -8,6 +8,15 @@ var handleMissingKeys = require('./validator').handleMissingKeys;
 var User = require('./handler.actions').create('User');
 var Order = require('./handler.actions').create('Order');
 
+function cbHell(quantity, cb) {
+    return {
+        call: () => cb(),
+        next: () => {
+            quantity--;
+            if (quantity === 0) cb();
+        }
+    }
+}
 
 function general(data, cb) {
     var rta = {};
@@ -16,15 +25,18 @@ function general(data, cb) {
         User.model.count({
             userType: 'client'
         }, (err, nroClients) => {
+            if(err) cb(null,rta);
             rta.nroClients = nroClients;
             User.model.count({
                 userType: 'diag'
             }, (err, nroDiags) => {
+                if(err) cb(null,rta);
                 rta.nroDiags = nroDiags;
                 User.model.count({
                     userType: 'client',
                     clientType: 'landlord'
                 }, (err, nro) => {
+                    if(err) cb(null,rta);
                     rta.nroLandlords = nro;
                     rta.nroAgencies = nroClients - rta.nroLandlords;
                     _orders();
@@ -33,28 +45,54 @@ function general(data, cb) {
         });
     }
 
+
+
     function _orders() {
+        var _handler = cbHell(5, () => {
+            cb(null, rta);
+        });
+        
+        Order.model.count({
+            status: 'created'
+        }, (err, nro) => {
+            if(err) cb(null,rta);
+            rta.nroOrdersCreated = nro;
+            _handler.next();
+        });
+
         Order.model.count({
             status: 'ordered'
         }, (err, nro) => {
+            if(err) cb(null,rta);
             rta.nroOrdersOrdered = nro;
-            Order.model.count({
-                status: 'prepaid'
-            }, (err, nro) => {
-                rta.nroOrdersPrepaid = nro;
-                Order.model.count({
-                    status: 'delivered'
-                }, (err, nro) => {
-                    rta.nroOrdersDelivered = nro;
-                    Order.model.count({
-                        status: 'completed'
-                    }, (err, nro) => {
-                        rta.nroOrdersCompleted = nro;
-                        cb(null,rta);
-                    });
-                });
-            });
+            _handler.next();
         });
+
+        Order.model.count({
+            status: 'prepaid'
+        }, (err, nro) => {
+            if(err) cb(null,rta);
+            rta.nroOrdersPrepaid = nro;
+            _handler.next();
+        });
+
+        Order.model.count({
+            status: 'delivered'
+        }, (err, nro) => {
+            if(err) cb(null,rta);
+            rta.nroOrdersDelivered = nro;
+            _handler.next();
+        });
+
+        Order.model.count({
+            status: 'completed'
+        }, (err, nro) => {
+            if(err) cb(null,rta);
+            rta.nroOrdersCompleted = nro;
+            _handler.next();
+        });
+
+
     }
 
     _users();
